5bf2adedc610d2d4e6c21795a0f8bf31fe8e8b42
Bogdan Georgiev <bobogeorgievv97@gmail.com>
2024-06-08 20:16:18 +0300

    adding touchstart and touchend on nav button

--- src/app.js
+++ src/app.js
@@ -13,3 +13,23 @@ document.querySelector('.logos').appendChild(copy)
 
 const partnersCopy = document.querySelector(".partners-slide").cloneNode(true);
 document.querySelector('.partners').appendChild(partnersCopy);
+
+// change color of button when pressed on mobile or tablet
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navButton = document.getElementById('nav-btn');
+
+    // Event listener for touchstart (mobile/tablet)
+    navButton.addEventListener('touchstart', function() {
+        navButton.classList.add('pressed');
+    });
+
+    // Event listener for touchend (mobile/tablet)
+    navButton.addEventListener('touchend', function() {
+        navButton.classList.remove('pressed');
+    });
+
+    navButton.addEventListener('mouseleave', function() {
+        navButton.classList.remove('pressed');
+    });
+});
